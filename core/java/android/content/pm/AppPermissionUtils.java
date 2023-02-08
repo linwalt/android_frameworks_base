925010e8387b522f67df3308fea1c559367fefc1
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-02-08 09:21:25 +0200

    don't bypass cache when spoofing INTERNET permission self-checks
    The next commit caches spoofed permission checks, which is incompatible with the previous
    approach to spoofing INTERNET checks.
    
    Squash into "improve compatibility of INTERNET special runtime permission"

--- core/java/android/content/pm/AppPermissionUtils.java
+++ core/java/android/content/pm/AppPermissionUtils.java
@@ -28,6 +28,7 @@ public class AppPermissionUtils {
                 && SpecialRuntimePermAppUtils.requestsInternetPermission()
                 && !SpecialRuntimePermAppUtils.awareOfRuntimeInternetPermission())
         {
+            SpecialRuntimePermAppUtils.isInternetPermissionCheckSpoofed = true;
             return true;
         }
 
