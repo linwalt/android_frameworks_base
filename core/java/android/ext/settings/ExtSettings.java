9570db87f153fd9205aa51a0dea3eaa938fbccf4
r3g_5z <june@girlboss.ceo>
2023-04-02 22:28:14 -0400

    attestation key provisioning server setting
    Co-authored-by: flawedworld <flawedworld@flawed.world>
    Signed-off-by: r3g_5z <june@girlboss.ceo>

--- core/java/android/ext/settings/ExtSettings.java
+++ core/java/android/ext/settings/ExtSettings.java
@@ -6,6 +6,9 @@ import java.util.Set;
 import static android.ext.settings.GnssConstants.SUPL_DISABLED;
 import static android.ext.settings.GnssConstants.SUPL_SERVER_GRAPHENEOS_PROXY;
 import static android.ext.settings.GnssConstants.SUPL_SERVER_STANDARD;
+import static android.ext.settings.RemoteProvisioningConstants.GRAPHENEOS_PROXY;
+import static android.ext.settings.RemoteProvisioningConstants.STANDARD_SERVER;
+import static android.ext.settings.RemoteProvisioningConstants.DISABLED;
 
 /** @hide */
 public class ExtSettings {
@@ -23,6 +26,12 @@ public class ExtSettings {
             Setting.Scope.GLOBAL, "force_disable_supl", // historical name
             SUPL_SERVER_GRAPHENEOS_PROXY, // default
             SUPL_SERVER_STANDARD, SUPL_DISABLED, SUPL_SERVER_GRAPHENEOS_PROXY // valid values
+    );
+
+    public static final IntSetting REMOTE_PROVISIONING_SERVER = new IntSetting(
+        Setting.Scope.GLOBAL, "attest_remote_provisioner_server", // historical setting key
+        GRAPHENEOS_PROXY, // default
+        STANDARD_SERVER, GRAPHENEOS_PROXY // valid values
     );
 
     private ExtSettings() {}
