4f202587610c85b53a9c757f26ab8f3bb09fabb1
walternewtz <walternewtz@outlook.com>
2023-05-26 12:21:44 +0530

    Revert "little change for spoof"
    This reverts commit e50cf1d70884662b70f0bf07c3ec2a60cd0cdfd3.

--- keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
+++ keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -42,7 +42,6 @@ import android.system.keystore2.ResponseCode;
 import android.util.Log;
 
 import com.android.internal.annotations.VisibleForTesting;
-import com.android.internal.baikalos.BaikalSpoofer;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -165,8 +164,7 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
     @Override
     public Certificate[] engineGetCertificateChain(String alias) {
-       BaikalSpoofer.onEngineGetCertificateChain();
-       KeyEntryResponse response = getKeyMetadata(alias);
+        KeyEntryResponse response = getKeyMetadata(alias);
 
         if (response == null || response.metadata.certificate == null) {
             return null;
